b789133f0825fdf620c451283344618f2445406e
DanielTrusca1 <marian.trusca@8x8.com>
2025-08-07 11:03:17 +0300

    Add error logs + error message in case input is not valid

--- src/MyForm.jsx
+++ src/MyForm.jsx
@@ -2,14 +2,40 @@ import React from "react";
 import { useForm } from "react-hook-form";
 
 export default function MyForm() {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  console.log(errors);
 
   return (
     <div>
-      <form onSubmit={handleSubmit((data) => {
-        console.log(data)
-      })}>
-        <input {...register("name")} placeholder="Name" />
+      <form
+        onSubmit={handleSubmit((data) => {
+          console.log(data);
+        })}
+      >
+        <div>
+          <input
+            {...register("name", {
+              required: "This field is required.",
+              minLength: {
+                value: 3,
+                message: "Minimum length is 3",
+              },
+              maxLength: 50,
+              pattern: {
+                value: /^[A-Za-z]+$/,
+                message: "Only letters are allowed",
+              },
+            })}
+            placeholder="Name"
+          />
+          <p>{errors.name?.message}</p>
+        </div>
+
         <input {...register("email")} placeholder="Email" />
         <input {...register("backup-email")} placeholder="Backup Email" />
         <input type="submit" />
