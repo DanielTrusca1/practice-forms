a748875e6e3f4395fb1bad63b94c95505b622afd
DanielTrusca1 <marian.trusca@8x8.com>
2025-08-07 15:54:11 +0300

    Add country flags + CSS style for the select element

--- src/SelectCountry.jsx
+++ src/SelectCountry.jsx
@@ -2,15 +2,28 @@ import Select from "react-select";
 import countryList from "react-select-country-list";
 
 import React, { useMemo } from "react";
-const SelectCountry = () => {
-  const options = useMemo(() => countryList().getData(), []);
 
+const getFlagEmoji = (isoCode) =>
+  isoCode
+    .toUpperCase()
+    .replace(/./g, (char) => String.fromCodePoint(127397 + char.charCodeAt()));
+
+const SelectCountry = () => {
+  const countries = useMemo(() => {
+    return countryList()
+      .getData()
+      .map((country) => ({
+        value: country.value,
+        label: `${getFlagEmoji(country.value)} ${country.label}`,
+      }));
+  }, []);
   return (
     <Select
       className="react-select"
       classNamePrefix="react-select"
-      options={options}
+      options={countries}
       isClearable
+      placeholder="Country"
     />
   );
 };
