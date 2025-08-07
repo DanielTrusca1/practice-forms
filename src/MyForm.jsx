57f6dcd4e7ffa5c55f30ef6217c6e89296cbb408
DanielTrusca1 <marian.trusca@8x8.com>
2025-08-07 10:52:51 +0300

    Register form inputs + extract and log form data on submit

--- src/MyForm.jsx
+++ src/MyForm.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import { useForm } from "react-hook-form";
 
 export default function MyForm() {
-  const { register } = useForm();
+  const { register, handleSubmit } = useForm();
 
   return (
     <div>
-      <form>
-        <input name="name" placeholder="Name" />
-        <input name="email" placeholder="Email" />
-        <input name="backup-email" placeholder="Backup Email" />
+      <form onSubmit={handleSubmit((data) => {
+        console.log(data)
+      })}>
+        <input {...register("name")} placeholder="Name" />
+        <input {...register("email")} placeholder="Email" />
+        <input {...register("backup-email")} placeholder="Backup Email" />
         <input type="submit" />
       </form>
     </div>
