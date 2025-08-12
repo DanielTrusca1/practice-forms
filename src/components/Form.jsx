30d945c3b25f958c0137b08d205112b53652d652
DanielTrusca1 <marian.trusca@8x8.com>
2025-08-12 13:56:42 +0300

    Wrap app content with redux provider

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,37 @@
+import { useSelector, useDispatch } from "react-redux";
+
+import { updateField, resetForm } from "../redux/formSlice";
+
 const Form = () => {
-  return <div>Form</div>;
+  const form = useSelector((state) => state.form);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="redux-form">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log(form);
+        }}
+      >
+        <input
+          value={form.name}
+          onChange={(e) =>
+            dispatch(updateField({ field: "name", value: e.target.value }))
+          }
+          placeholder="Name"
+        />
+        <input
+          value={form.email}
+          onChange={(e) =>
+            dispatch(updateField({ field: "email", value: e.target.value }))
+          }
+          placeholder="Email"
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default Form;
