0c8194b0bf21782bfc1ebbb5ba65b2c6ae0c8730
DanielTrusca1 <marian.trusca@8x8.com>
2025-08-13 17:06:30 +0300

    Create custom input component + pass props from redux <Field>

---
+++ src/components/CustomInput.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomInput = () => {
+  return (
+    <input type="text"/>
+  )
+}
+
+export default CustomInput
--- src/components/FormExample.jsx
+++ src/components/FormExample.jsx
@@ -1,21 +1,21 @@
 import { reduxForm, Field } from "redux-form";
 
+import CustomInput from "./CustomInput";
+
 // Custom input field JSX example
-const customInput = (props) => (
-  <input type="text" />
-)
+const customInput = (props) => <CustomInput {...props.input} type="text" />;
 
 // Form JSX example
 const Form = () => (
   <div>
     <h1>Redux Form</h1>
     <form>
-      <Field name="name" component={customInput}/>
+      <Field name="name" component={customInput} />
       <button type="submit">Submit</button>
     </form>
   </div>
 );
 
 export default reduxForm({
-  form: "My Redux Form Example"
+  form: "My Redux Form Example",
 })(Form);
