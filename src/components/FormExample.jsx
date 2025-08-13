bd9e9079e85bf7b83529431551d59173f1381181
DanielTrusca1 <marian.trusca@8x8.com>
2025-08-13 17:15:29 +0300

    Add field-level validation for required input

--- src/components/FormExample.jsx
+++ src/components/FormExample.jsx
@@ -5,17 +5,30 @@ import CustomInput from "./CustomInput";
 // Custom input field JSX example
 const customInput = (props) => <CustomInput {...props.input} type="text" />;
 
+const onSubmit = (values) => {
+  console.log(JSON.stringify(values));
+};
+
+const required = (val) => {
+  if (!val || val === "") return "This field is required.";
+
+  return undefined;
+};
+
 // Form JSX example
-const Form = () => (
-  <div>
-    <h1>Redux Form</h1>
-    <form>
-      <Field name="name" component={customInput} />
-      <button type="submit">Submit</button>
-    </form>
-  </div>
-);
+const Form = ({ handleSubmit }) => {
+  return (
+    <div>
+      <h1>Redux Form</h1>
+      <form onSubmit={handleSubmit}>
+        <Field name="name" component={customInput} validate={required}/>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default reduxForm({
   form: "My Redux Form Example",
+  onSubmit,
 })(Form);
