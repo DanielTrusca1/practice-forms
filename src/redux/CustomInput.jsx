4e531b8b45baa8670e8f84a4916118f3a87412ea
DanielTrusca1 <marian.trusca@8x8.com>
2025-08-14 14:03:38 +0300

    Add push and remove functionality to hobbies list field

--- src/redux/CustomInput.jsx
+++ src/redux/CustomInput.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const CustomInput = ({ input, label, meta, ...rest }) => {
   return (
     <div>
-      <label>{label}:</label>
-      <input {...input} {...rest} placeholder={label}/>
+      {label !== "none" && <label>{label}:</label>}
+      <input {...input} {...rest} placeholder={label === "none" ? "" : label} />
       {meta.touched && meta.error && <span>{meta.error}</span>}
     </div>
   );
--- src/redux/HobbiesInputArray.jsx
+++ src/redux/HobbiesInputArray.jsx
@@ -6,28 +6,32 @@ import CustomInput from "./CustomInput";
 const HobbiesInputArray = ({ fields }) => {
   return (
     <div className="redux-hobbies-field">
+      <label>Hobbies:</label>
       {fields.map((name, index) => (
         <div key={index}>
           <Field
             name={name}
-            label={name}
+            label="none"
             component={CustomInput}
             type="text"
             placeholder={`Hobby #${index + 1}`}
           />
-          <button type="button" onClick={() => fields.remove(index)}>
+          <button
+            type="button"
+            onClick={() => fields.remove(index)}
+            style={{ backgroundColor: "rgb(255, 100, 100)" }}
+          >
             x
           </button>
         </div>
       ))}
 
-      <Field
-        name="addHobby"
-        label="Add Hobby"
-        component={CustomInput}
-        type="text"
-      />
-      <button type="button" onClick={() => fields.push()}>
+      <Field name="addHobby" label="none" component={CustomInput} type="text" />
+      <button
+        type="button"
+        onClick={() => fields.push()}
+        style={{ backgroundColor: "rgb(30, 200, 30)" }}
+      >
         +
       </button>
     </div>
