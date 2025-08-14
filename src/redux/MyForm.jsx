d90015eaf8451ea7c8c7b36ba75b4d4ad8b6e2b5
DanielTrusca1 <marian.trusca@8x8.com>
2025-08-14 11:31:54 +0300

    Link css module to the new redux form

--- src/redux/MyForm.jsx
+++ src/redux/MyForm.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 const MyForm = ({ handleSubmit }) => (
-  <form onSubmit={handleSubmit((values) => console.log(values))}>
-    <Field
-      name="username"
-      component="input"
-      type="text"
-      placeholder="Username"
-    />
-    <button type="submit">Submit</button>
-  </form>
+  <div className="redux-form">
+    <form onSubmit={handleSubmit((values) => console.log(values))}>
+      <Field
+        name="username"
+        component="input"
+        type="text"
+        placeholder="Username"
+      />
+      <button type="submit">Submit</button>
+    </form>
+  </div>
 );
 
 export default reduxForm({ form: "My Redux Form" })(MyForm);
