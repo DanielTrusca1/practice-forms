d4ad57297b8031568da9c14b9a75aa4f379248b4
DanielTrusca1 <marian.trusca@8x8.com>
2025-08-14 16:53:31 +0300

    Enable backup-email autofill

--- src/redux/CustomInput.jsx
+++ src/redux/CustomInput.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 
-const CustomInput = ({ input, label, meta, ...rest }) => {
+const CustomInput = ({ input, label, meta, onBlurAdditional, ...rest }) => {
+  const handleBlur = (e) => {
+    if (onBlurAdditional) onBlurAdditional(e);
+
+    input.onBlur(e);
+  };
+
   return (
     <div>
       {label !== "none" && <label>{label}:</label>}
-      <input {...input} {...rest} placeholder={label === "none" ? "" : label} />
-      {meta.touched && meta.error && <span>{meta.error}</span>}
+      <input
+        {...input}
+        {...rest}
+        placeholder={label === "none" ? "" : label}
+        onBlur={handleBlur}
+      />
+      {meta.touched && meta.error && (
+        <span className="validation-message">{meta.error}</span>
+      )}
     </div>
   );
 };
--- src/redux/MyForm.jsx
+++ src/redux/MyForm.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-import { Field, FieldArray, reduxForm } from "redux-form";
+import { Field, FieldArray, reduxForm, change } from "redux-form";
 
 import CustomInput from "./CustomInput";
 import SelectCountry from "../SelectCountry";
 import HobbiesInputArray from "./HobbiesInputArray";
+
+// Dispatch actions to reducers
+import { useDispatch } from "react-redux";
 
 const required = (value) => (value ? undefined : "Required");
 
@@ -21,40 +24,54 @@ const validEmail = (value) =>
     ? undefined
     : "Invalid email adress";
 
-const MyForm = ({ handleSubmit }) => (
-  <div className="redux-form">
-    <form onSubmit={handleSubmit((values) => console.log(values))}>
-      <Field
-        name="Name"
-        label="Name"
-        component={CustomInput} /// Use the custom input component instead of the default !
-        type="text"
-        validate={[required, minLength(3), maxLength(50), onlyLetters]}
-      />
-      <Field name="email" label="Email" component={CustomInput} type="email" />
-      <Field
-        name="backupEmail"
-        label="Backup-Email"
-        component={CustomInput}
-        type="email"
-        validate={validEmail}
-      />
+const MyForm = ({ handleSubmit }) => {
+  // Extract dispatch object
+  const dispatch = useDispatch();
 
-      <SelectCountry />
+  
 
-      <FieldArray name="hobbies" component={HobbiesInputArray} />
+  return (
+    <div className="redux-form">
+      <form onSubmit={handleSubmit((values) => console.log(values))}>
+        <Field
+          name="Name"
+          label="Name"
+          component={CustomInput} /// Use the custom input component instead of the default !
+          type="text"
+          validate={[required, minLength(3), maxLength(50), onlyLetters]}
+        />
+        <Field
+          name="email"
+          label="Email"
+          component={CustomInput}
+          type="text"
+          validate={validEmail}
+          onBlurAdditional={(e) => dispatch(change("My Redux Form", "backupEmail", e.target.value))}
+        />
+        <Field
+          name="backupEmail"
+          label="Backup-Email"
+          component={CustomInput}
+          type="text"
+          validate={validEmail}
+        />
 
-      <Field
-        name="username"
-        label="Username"
-        component={CustomInput}
-        type="text"
-        validate={validEmail}
-      />
-      <button type="submit">Submit</button>
-    </form>
-  </div>
-);
+        <SelectCountry />
+
+        <FieldArray name="hobbies" component={HobbiesInputArray} />
+
+        <Field
+          name="username"
+          label="Username"
+          component={CustomInput}
+          type="text"
+          validate={validEmail}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default reduxForm({
   form: "My Redux Form",
